a023b65cd1f4cc90e8e8d16d90b205a3439a39ff
shriyansh19 <shriyanshkesharwani3162@gmail.com>
2025-03-01 12:22:54 +0530

    [Shriyansh Kesharwani] Add . Problem 3: Use REPL – Add two Random Dice Numbers and Print the Result

---
+++ 01 - JS Programming Constructs/Sequence Practice Problems/Problem3.js
@@ -0,0 +1,13 @@
+// Problem 3: Use REPL – Add two Random Dice Numbers and Print the Result
+
+// Generate two random dice numbers
+let dice1 = Math.floor(Math.random() * 6) + 1;
+let dice2 = Math.floor(Math.random() * 6) + 1;
+
+// Add the two dice numbers
+let sumOfDice = dice1 + dice2;
+
+// Print the result
+console.log("Dice 1:", dice1);
+console.log("Dice 2:", dice2);
+console.log("Sum of Two Dice Numbers:", sumOfDice);
