680e7566c23812de446e5fdd1bddecfc9f5365f4
shriyansh19 <shriyanshkesharwani3162@gmail.com>
2025-03-01 12:21:01 +0530

    [Shriyansh Kesharwani] Add . Problem 2: Use REPL – Use Random to get Dice Number between 1 to 6

---
+++ 01 - JS Programming Constructs/Sequence Practice Problems/Problem2.js
@@ -0,0 +1,5 @@
+// Problem 2: Use REPL – Use Random to get Dice Number between 1 to 6
+
+// Generate a random number between 1 and 6 (inclusive) for a dice roll
+let diceNumber = Math.floor(Math.random() * 6) + 1;
+console.log("Random Dice Number:", diceNumber);
