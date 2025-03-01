6b6143a40c5223845f31b6ff4aa90ba9886d26fe
shriyansh19 <shriyanshkesharwani3162@gmail.com>
2025-03-01 12:15:57 +0530

    [Shriyansh Kesharwani] Add . Problem 1: Use REPL – Random Function Math.floor(Math.random() * 10); to get a Single Digit

---
+++ 01 - JS Programming Constructs/Sequence Practice Problems/Problem1.js
@@ -0,0 +1,5 @@
+// Problem 1: Use REPL – Random Function Math.floor(Math.random() * 10); to get a Single Digit
+
+// Generate a random single digit (0-9) using Math.random()
+let randomDigit = Math.floor(Math.random() * 10);
+console.log("Random Single Digit:", randomDigit);
